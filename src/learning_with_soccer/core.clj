ea72a8449fa4f83cbf0d0e28a6eaa648b02be016
Natan Macedo <natan.macedo@natan.macedo>
2023-11-06 10:57:17 -0300

    making code more readable

--- src/learning_with_soccer/core.clj
+++ src/learning_with_soccer/core.clj
@@ -1,66 +1,50 @@
-(ns learning-with-soccer.core)
-
+(ns testproject.core
+  (:require [clojure.string]))
 
 (defn make-team
   "Create a team"
-  [id, fullname, know-as, abbreviation] {
-                                         :team-id      id
-                                         :fullname     fullname
-                                         :know-as      know-as
-                                         :abbreviation abbreviation})
+  [id, abbreviation, full-name, know-as] {:team-id      id
+                                          :abbreviation abbreviation
+                                          :full-name    full-name
+                                          :know-as      know-as})
 
 
-(def all-teams [(make-team (random-uuid) "Santos Futebol Clube" "Santos" "SAN")
-                (make-team (random-uuid) "Palmeiras" "Palmeiras" "PAL")
-                (make-team (random-uuid) "São Paulo" "São Paulo" "SPO" )
-                (make-team (random-uuid) "Corinthians" "Corinthians" "COR" )
-                (make-team (random-uuid) "Clube de Regatas Flamengo" "Flamengo" "FLA" )])
+(def all-teams [(make-team (random-uuid) "SAN" "Santos Futebol Clube" "Santos")
+                (make-team (random-uuid) "PAL" "Palmeiras" "Palmeiras")
+                (make-team (random-uuid) "SPO" "São Paulo" "São Paulo")
+                (make-team (random-uuid) "COR" "Corinthians" "Corinthians")
+                (make-team (random-uuid) "FLA" "Clube de Regatas Flamengo" "Flamengo")])
 
-
-(defn get-team [id] (->> all-teams
-                         (filter #(= (:team-id %) id))
-                         (first)
-                         :know-as))
+(defn get-team
+  [id]
+  (->> all-teams
+       (filter #(= (:team-id %) id))
+       (first)))
 
 (defn make-match
-  "Create a match with a home team and an away team"
-  [id home-team-id away-team-id]
-  {
-   :match-id  id
-   :home-team home-team-id
-   :away-team  away-team-id
-   :status    :not-started })
+  [id home-team-id away-team-id] {:match-id  id
+                                  :home-team-id home-team-id
+                                  :away-team-id  away-team-id
+                                  :status    :not-started})
 
-(defn print-match
+(defn match-str
   [match]
-  (println (get-team (:home-team match)) "x" (get-team (:away-team match)))
-  match
-  )
+  (let [home-team (:know-as (get-team (:home-team-id match)))
+        away-team (:know-as (get-team (:away-team-id match)))]
+    (clojure.string/join " " [home-team "x" away-team])))
 
 (defn round-robin
-  "Create matches with all teams in :teams"
-  [acc, team]
-  (let [
-        teams (filter #(not (= (:team-id team) (:team-id %))) (:teams acc))
-        matches (:matches acc)
-        new-matches (map #(make-match (random-uuid) (:team-id team) (:team-id %)) teams)
-        ] {
-           :matches (concat new-matches matches)
-           :teams teams
-           }))
-
+  [matched-teams, team]
+  (let [eligible-teams (filter #(not (= (:team-id team) (:team-id %))) (:teams matched-teams))
+        matches (:matches matched-teams)
+        new-matches (map #(make-match (random-uuid) (:team-id team) (:team-id %)) eligible-teams)] {:matches (concat new-matches matches)
+                                                                                                    :teams eligible-teams}))
 
 (defn generate-tournament
   [teams]
-  (let [round-count (- (count teams) 1)
-        matches (:matches (reduce round-robin {:matches [] :teams teams} teams))]
-    (println (map #(print-match %) matches))))
+  (let [reduced (reduce round-robin {:matches [] :teams teams} teams)
+        matches (:matches reduced)]
+    (map #(match-str %) matches)))
 
-(generate-tournament all-teams)
-
-
-(defn- set-goal-home
-  [match goals] (assoc :goals-home goals match))
-
-(defn- set-goal-away
-  [match goals] (assoc :goals-away goals match))
+(def tournament-str (clojure.string/join "\n" (generate-tournament all-teams)))
+(println tournament-str)
